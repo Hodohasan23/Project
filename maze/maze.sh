66f4a974aa0aa5943e8c37b7fc9b608b4c1cde0d
Hodohasan23 <146748008+Hodohasan23@users.noreply.github.com>
2024-03-14 13:23:14 +0000

    nearly finished

--- maze/maze.sh
+++ maze/maze.sh
@@ -3,7 +3,7 @@
 # remove read permissions from this file to perfrom Bad permissions test
 chmod -r Files/bad_permissions.txt
 
-gcc mazeGame.c -o mazeGame
+gcc maze.c -o maze
 
 
 echo -e "~~ Maze Game Tests ~~"
@@ -12,7 +12,7 @@ echo -e "~~ File handling ~~"
 
 # FirstMaze.txt is used to test that file loads succesfully
 echo -n "Testing a succesfull file is loaded  - "
-./mazeGame Files/FirstMaze.txt > tmp
+./maze Files/FirstMaze.txt > tmp
 if grep -q "File Files/FirstMaze.txt has been loaded successfully" tmp;
 then
     echo "PASS"
@@ -22,7 +22,7 @@ fi
 
 #Testing a file with invalid data 
 echo -n "Testing an invalid file- "
-./mazeGame Files/invalid_file.txt > tmp
+./maze Files/invalid_file.txt > tmp
 if grep -q "Error: txt file is in an incorrect format" tmp; then
     echo "PASS"
 else
@@ -31,7 +31,7 @@ fi
 
 # This tests if the data has missing fields, No S ( Start Point ) - bad_data_missing_s has no start point
 echo -n "Testing when there is a missing starting point - "
-timeout 0.2s ./mazeGame Files/Missing_end.txt > tmp
+timeout 0.2s ./maze Files/Missing_end.txt > tmp
 if grep -q "Error: txt file is in an incorrect format" tmp;
 then 
     echo "PASS"
@@ -41,7 +41,7 @@ fi
 
 # Testing to see if there are any other characters that not the correct # S E, which would make the maze invalid the file used in test has others than the default three.
 echo -n "Testing bad data whith incorrect characters - "
-timeout 0.2s ./mazeGame Files/Incorrect_Characters.txt  > tmp
+timeout 0.2s ./maze Files/Incorrect_Characters.txt  > tmp
 if grep -q "Error: txt file is in an incorrect format" tmp;
 then
     echo "PASS"
@@ -51,7 +51,7 @@ fi
 
 # This tests if the data has missing fields, No E ( End Point ) - bad_data_missing_e has no end point
 echo -n "Testing when there is a missing ending point - "
-timeout 0.2s ./mazeGame Files/Missing_start.txt  > tmp
+timeout 0.2s ./maze Files/Missing_start.txt  > tmp
 if grep -q "Error: txt file is in an incorrect format" tmp;
 then 
     echo "PASS"
@@ -62,7 +62,7 @@ fi
 
 # This tests a the file non_existent_file.txt which does not exist or has incorrect file name
 echo -n "Testing a file that has incorrect name or doesn't exist- "
-./mazeGame Files/non_existent_file.txt > tmp
+./maze Files/non_existent_file.txt > tmp
 if grep -q "Error: Incorrect filename " tmp; then
     echo "PASS"
 else
@@ -70,7 +70,7 @@ else
 fi
 
 echo -n "Testing a file that does not exist- "
-./mazeGame Files/non_existent_file.txt > tmp
+./maze Files/non_existent_file.txt > tmp
 if grep -q "Error: Cannot open file" tmp; then
     echo "PASS"
 else
@@ -79,7 +79,7 @@ fi
 
 # Testing loading an empty maze file
 echo -n "Testing loading an empty maze file - "
-./mazeGame Files/empty_file.txt > tmp
+./maze Files/empty_file.txt > tmp
 if grep -q "Error: File is empty" tmp; then
     echo "PASS"
 else
@@ -89,8 +89,8 @@ fi
 echo -e "~~ Argument Tests ~~"
 
 echo -n "Testing no arguments - "
-./mazeGame > tmp
-if grep -q "Usage: ./mazeGame <maze_file>" tmp;
+./maze > tmp
+if grep -q "Usage: ./maze <maze_file>" tmp;
 then
     echo "PASS"
 else
@@ -98,8 +98,8 @@ else
 fi
 
 echo -n "Testing 2 arguments - "
-./mazeGame x x > tmp
-if grep -q "Usage: ./mazeGame <maze_file>" tmp;
+./maze x x > tmp
+if grep -q "Usage: ./maze <maze_file>" tmp;
 then
     echo "PASS"
 else
@@ -110,7 +110,7 @@ echo -e "~~ Movement testing ~~"
 
 #This tests movement logic if user has multiple pathways to choose from
 echo -n "Testing file with multiple path ways - "
-./mazeGame Files/multiple_pathways.txt > tmp
+./maze Files/multiple_pathways.txt > tmp
 if grep -q "Congratulations - succefully exited the maze" tmp;
 then
     echo "PASS"
@@ -120,7 +120,7 @@ fi
 
 #This tests movement logic if user is in a maze with a reptitive structure
 echo -n "Testing file with pathways that have a repetitive structure - "
-./mazeGame Files/repetitive_pathway.txt > tmp
+./maze Files/repetitive_pathway.txt > tmp
 if grep -q "Congratulations - succefully exited the maze" tmp;
 then
     echo "PASS"
@@ -130,7 +130,7 @@ fi
 
 #This tests multiple different exits in file to see how mulitple wining conditions are handled
 echo -n "Testing maze file with multiple exits - "
-./mazeGame Files/Multiple_exits.txt > tmp
+./maze Files/Multiple_exits.txt > tmp
 if grep -q "Congratulations - succefully exited the maze" tmp;
 then
     echo "PASS"
@@ -140,7 +140,7 @@ fi
 
 #Testing when a user tries to move beyond the maze structure
 echo -n "Testing user attempt to move beyond the maze - "
-./mazeGame Files/Beyond_edge.txt > tmp
+./maze Files/Beyond_edge.txt > tmp
 if grep -q "Error - cant move beyond the maze" tmp;
 then
     echo "PASS"
@@ -150,7 +150,7 @@ fi
 
 #This test is done to simulate a scenario where user runs into a wall
 echo -n "Testing when user moves into a wall - "
-./mazeGame Files/Wall.txt > tmp
+./maze Files/Wall.txt > tmp
 if grep -q "Error - User moved in to a wall (#) cannot be done" tmp;
 then
     echo "PASS"
@@ -160,7 +160,7 @@ fi
 
 #This tests movement logic if user enters they key 'A' in uppercase they should move leftwards and would be displayed a success message.
 echo -n "Testing leftward movement 'A' - "
-echo "A" | ./mazeGame Files/Leftward.txt > tmp
+echo "A" | ./maze Files/Leftward.txt > tmp
 if grep -q "Congratulations - succefully exited the maze" tmp;
 then
     echo "PASS"
@@ -170,7 +170,7 @@ fi
 
 #This tests movement logic if user enters they key 'D' in uppercase they should move rightwards and would be displayed a success message.
 echo -n "Testing rightward movement 'D' - "
-echo "D" | ./mazeGame Files/Rightward.txt > tmp
+echo "D" | ./maze Files/Rightward.txt > tmp
 if grep -q "Congratulations - succefully exited the maze" tmp;
 then
     echo "PASS"
@@ -180,7 +180,7 @@ fi
 
 #This tests movement logic if user enters they key 'W' in uppercase they should move upwards and would be displayed a success message.
 echo -n "Testing upward movement'W'- "
-echo "W" | ./mazeGame Files/Upward.txt > tmp
+echo "W" | ./maze Files/Upward.txt > tmp
 if grep -q "Congratulations - succefully exited the maze" tmp;
 then
     echo "PASS"
@@ -190,7 +190,7 @@ fi
 
 #This tests movement logic if user enters they key 'S' in uppercase they should move downwards and would be displayed a success message.
 echo -n "Testing downward movement 'S' - "
-echo "S" | ./mazeGame Files/Downward.txt > tmp
+echo "S" | ./maze Files/Downward.txt > tmp
 if grep -q "Congratulations - succefully exited the maze" tmp;
 then
     echo "PASS"
@@ -200,7 +200,7 @@ fi
 
 #This tests movement logic if user enters they key 'a' in lowercase they should move leftwards and would be displayed a success message.
 echo -n "Testing leftward movement 'a' - "
-echo "a" | ./mazeGame Files/Leftward.txt > tmp
+echo "a" | ./maze Files/Leftward.txt > tmp
 if grep -q "Congratulations - succefully exited the maze" tmp;
 then
     echo "PASS"
@@ -210,7 +210,7 @@ fi
 
 #This tests movement logic if user enters they key 'd' in lowercase they should move rightwards and would be displayed a success message.
 echo -n "Testing rightward movement 'd'- "
-echo "d" | ./mazeGame Files/Rightward.txt > tmp
+echo "d" | ./maze Files/Rightward.txt > tmp
 if grep -q "Congratulations - succefully exited the maze" tmp;
 then
     echo "PASS"
@@ -220,7 +220,7 @@ fi
 
 #This tests movement logic if user enters they key 'w' in lowercase they should move upwards and would be displayed a success message.
 echo -n "Testing upward movement 'w' - "
-echo "w" | ./mazeGame Files/Upward.txt > tmp
+echo "w" | ./maze Files/Upward.txt > tmp
 if grep -q "Congratulations - succefully exited the maze" tmp;
 then
     echo "PASS"
@@ -230,7 +230,7 @@ fi
 
 #This tests movement logic if user enters they key 's' in lowercase they should move downwards and would be displayed a success message.
 echo -n "Testing downward movement 's' - "
-echo "s" | ./mazeGame Files/Downward.txt > tmp
+echo "s" | ./maze Files/Downward.txt > tmp
 if grep -q "Congratulations - succefully exited the maze" tmp;
 then
     echo "PASS"
@@ -240,8 +240,68 @@ fi
 
 #This is another way of testing movement where all keys regardless of case that are inputed are tested
 echo -n "User inputs correct letter for movement- "
-./mazeGame FirstMaze.txt < Inputs/InputsUser.txt > tmp
+./maze FirstMaze.txt < Inputs/InputsUser.txt > tmp
 if grep -q "Congratulations - successfully exited the maze" tmp; then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+echo -e "~~ Testing bad data ~~"
+
+# Testing if the height of the file for the maze range is above the maximum 
+echo -n "Testing if data is above maximum height - "
+timeout 0.2s ./maze files/Bad_data_height_exceeds.txt > tmp
+if grep -q "Error: txt file is in an incorrect format" tmp;
+then 
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+# Testing if the width of the file for the maze range is above the maximum 
+echo -n "Testing if data is above the maximum width - "
+timeout 0.2s ./maze files/Bad_data_width_exceeds.txt > tmp
+if grep -q "Error: txt file is in an incorrect format" tmp;
+then 
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+# Testing if the height of the file for the maze range is below the minimum
+echo -n "Testing if data is below minimum height - "
+timeout 0.2s ./maze files/BadData_below_minHeight.txt > tmp
+if grep -q "Error: txt file is in an incorrect format" tmp;
+then 
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+# Testing if the width of the file for the maze range is below the minimum
+echo -n "Testing if data is below the minimum width - "
+timeout 0.2s ./maze files/BadData_below_minWidth.txt > tmp
+if grep -q "Error: txt file is in an incorrect format" tmp;
+then 
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+# Test how game handles files with varying lengths in height
+echo -n "Testing if data has irregular height - "
+timeout 0.2s ./maze files/IrregularHeight.txt > tmp
+if grep -q "Error: txt file is in an incorrect format" tmp;
+then 
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+# Test how game handles files with varying lengths in width
+echo -n "Testing if data has irregular width - "
+timeout 0.2s ./maze files/IrregularWidth.txt > tmp
+if grep -q "Error: txt file is in an incorrect format" tmp;
+then 
     echo "PASS"
 else
     echo "FAIL"
@@ -251,7 +311,7 @@ echo -e "~~ Display of map testing ~~"
 
 # This tests searches for for a message of map being shown when user selects key 'M' and then presents success message
 echo -n "Testing Viewing of Image of Map - "
-echo "M" | ./mazeGame Files/FirstMaze.txt > tmp
+echo "M" | ./maze Files/FirstMaze.txt > tmp
 if grep -q "Successfully displaying image of Map" tmp;
 then
     echo "PASS"
@@ -261,7 +321,7 @@ fi
 
 # This tests searches for for a message of map being shown when user selects key 'm' and then presents success message
 echo -n "Testing Viewing of Image of Map - "
-echo "m" | ./mazeGame Files/FirstMaze.txt > tmp
+echo "m" | ./maze Files/FirstMaze.txt > tmp
 if grep -q "Successfully displaying image of Map" tmp;
 then
     echo "PASS"
@@ -273,7 +333,7 @@ fi
 echo -e "~~ User inputs bad data ~~"
 
 echo -n "User inputs incorrect movement key - "
-echo "" | ./mazeGame > tmp
+echo "" | ./maze > tmp
 if grep -q "Error: Movement character key is invalid" tmp;
 then
     echo "PASS"
@@ -283,7 +343,7 @@ fi
 
 #Test if user enters incorrect key that is not 'w a s d' they key 'l' is use for this test
 echo -n "User does not input anything - "
-echo "l" | ./mazeGame FirstMaze.txt > tmp
+echo "l" | ./maze FirstMaze.txt > tmp
 if grep -q "Error: Movement character key is invalid" tmp;
 then
     echo "PASS"
@@ -292,7 +352,7 @@ else
 fi
 
 echo -n "User does not input anything - "
-./mazeGame FirstMaze.txt < Inputs/noInput.txt > tmp
+./maze FirstMaze.txt < Inputs/noInput.txt > tmp
 if grep -q "Error: Movement character key is invalid" tmp; then
     echo "PASS"
 else

05a000838b828565eef49cf0e30d74b08a49e928
Hodohasan23 <146748008+Hodohasan23@users.noreply.github.com>
2024-05-15 22:27:52 +0000

    almost

--- maze/maze.h
+++ maze/maze.h
@@ -4,27 +4,27 @@
 #include <stdbool.h>
 
 #define MAXIMUM_SIZE 100
-#define MINIMUM_SIZE 5
-
-#define WALL '#'
-#define SPACE ' '
-#define START 'S'
-#define END 'E'
 
 typedef struct {
-    int x, y;
-} Player;
-
-typedef struct {
-    char grid[MAXIMUM_SIZE][MAXIMUM_SIZE];
-    int width, height;
+    char Size[MAXIMUM_SIZE][MAXIMUM_SIZE];
+    int width;
+    int height;
 } Maze;
+
+typedef struct {
+    // Define player attributes (e.g., position)
+    int x;
+    int y;
+} Player;
 
 bool mazeValidation(const Maze* maze);
 int loadMaze(Maze* maze, const char *filename);
 bool playerMovement(Player* player, const Maze* maze, char direction);
 void displayMap(const Maze* maze, const Player* player);
-void initializePlayer(const Maze* maze, Player* player);
-void gameLoop(const Maze* maze, Player* player);
+void locateStart(Player* player, const Maze* maze);
+bool locateEnd(const Player* player, const Maze* maze);
+void displayMaze(const Maze* maze);
+int helpScreen();
 
 #endif // MAZE_H
+
